86f21d1522848054de3d66aaf04c8f77b4d60582
CrosscutX <crosscutcode@gmail.com>
2023-02-28 20:49:42 -0500

    Main page design finished

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # quizzical
 
-Quiz app that pulls from Open Trivia API to dynamically render a quiz app
+Quiz app that pulls from Open Trivia API to dynamically render a quiz app. Design provided form the scrimba react curriculum.
 
 ## Technology
 
--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,5 +1,17 @@
 import { useState } from "react";
+import blob1 from "./assets/blob1.png";
+import blob2 from "./assets/blob2.png";
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="app">
+      <img src={blob1} alt="blob1" className="blob1" />
+      <div className="main-center">
+        <h1>Quzzical</h1>
+        <p>A short 5 question quiz</p>
+        <button>Start quiz</button>
+      </div>
+      <img src={blob2} alt="blob2" className="blob2" />
+    </div>
+  );
 }
