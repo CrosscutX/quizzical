8f03d5a5cb001043f2c4f1422316dbd263be6afd
CrosscutX <crosscutcode@gmail.com>
2023-02-28 22:42:17 -0500

    game styling functions

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import blob1 from "./assets/blob1.png";
 import blob2 from "./assets/blob2.png";
 import Game from "./components/Game";
 
 export default function App() {
   const [gameStart, setGameStart] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  function startGame() {
+    setGameStart(true);
+  }
+
+  useEffect(() => {
+    async function getQuestions() {
+      const res = await fetch(
+        "https://opentdb.com/api.php?amount=5&type=multiple"
+      );
+      const data = await res.json();
+      console.log(data.results);
+      setQuestions(data.results);
+    }
+    getQuestions();
+  }, [gameStart]);
 
   return (
     <div className="app">
@@ -13,10 +30,10 @@ export default function App() {
         <div className="main-center">
           <h1>Quzzical</h1>
           <p>A short 5 question quiz</p>
-          <button>Start quiz</button>
+          <button onClick={startGame}>Start quiz</button>
         </div>
       ) : (
-        <Game />
+        <Game questions={null} answer={null} correctAnswers={null} />
       )}
       <img src={blob2} alt="blob2" className="blob2" />
     </div>
--- quiz-app/src/components/Game.jsx
+++ quiz-app/src/components/Game.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function Game() {
-  return <div>Can't stop won't stop gamestop</div>;
+export default function Game(props) {
+  return (
+    <div className="game">
+      <div className="question-container">
+        <h2>How would one say goodbye in Spanish!</h2>
+        <div className="button-container">
+          <button>Adios</button>
+          <button>Hola</button>
+          <button>Cabbage Patch Kids</button>
+          <button>Salir</button>
+        </div>
+      </div>
+      <button className="check-answer-btn">Check answers</button>
+    </div>
+  );
 }
