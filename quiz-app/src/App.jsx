d08aac10622918a695db0580b8dcd589341747c6
CrosscutX <crosscutcode@gmail.com>
2023-02-28 21:24:09 -0500

    Conditional rendering on app/game

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import blob1 from "./assets/blob1.png";
 import blob2 from "./assets/blob2.png";
+import Game from "./components/Game";
 
 export default function App() {
+  const [gameStart, setGameStart] = useState(false);
+
   return (
     <div className="app">
       <img src={blob1} alt="blob1" className="blob1" />
-      <div className="main-center">
-        <h1>Quzzical</h1>
-        <p>A short 5 question quiz</p>
-        <button>Start quiz</button>
-      </div>
+      {gameStart === false ? (
+        <div className="main-center">
+          <h1>Quzzical</h1>
+          <p>A short 5 question quiz</p>
+          <button>Start quiz</button>
+        </div>
+      ) : (
+        <Game />
+      )}
       <img src={blob2} alt="blob2" className="blob2" />
     </div>
   );
---
+++ quiz-app/src/components/Game.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Game() {
+  return <div>Can't stop won't stop gamestop</div>;
+}
