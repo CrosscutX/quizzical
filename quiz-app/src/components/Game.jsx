bba09d661061504ba5beb98f035e56ea3f213ebd
CrosscutX <crosscutcode@gmail.com>
2023-03-01 22:13:02 -0500

    Created answers array

--- quiz-app/src/components/Game.jsx
+++ quiz-app/src/components/Game.jsx
@@ -6,16 +6,23 @@ export default function Game(props) {
       .replaceAll("&quot;", '"')
       .replaceAll("&#039;", "'")
       .replaceAll("&eacute;", "é")
-      .replaceAll("&amp;", "&");
+      .replaceAll("&amp;", "&")
+      .replaceAll("&rsquo;", "`");
   }
 
   const questionList = props.questions.map((question) => {
     //This replaces the weird syntax with the proper quotes.
-    let newQuestion = filterText(question.question);
-    let correctAnswer = filterText(question.correct_answer);
-
+    const newQuestion = filterText(question.question);
+    const correctAnswer = filterText(question.correct_answer);
+    const incorrectAnswers = question.incorrect_answers.map((answer) => {
+      return filterText(answer);
+    });
+    let allAnswers = [];
+    allAnswers.push(correctAnswer);
+    allAnswers.push(...incorrectAnswers);
+    console.log(allAnswers);
     return (
-      <div key={nanoid()} className="question-container">
+      <div key={nanoid()} className={"question-container"}>
         <h2>{newQuestion}</h2>
         <div className="button-container"></div>
       </div>
