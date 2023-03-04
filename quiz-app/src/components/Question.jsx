cc278858b8995feeea3886ce35fed1258294db46
CrosscutX <crosscutcode@gmail.com>
2023-03-03 21:09:16 -0500

    selected-btn class properly added

--- quiz-app/src/components/Question.jsx
+++ quiz-app/src/components/Question.jsx
@@ -1,16 +1,7 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Question(props) {
-  //This replaces the weird html entity syntax with the proper syntax.
-  function filterText(text) {
-    return text
-      .replaceAll("&quot;", '"')
-      .replaceAll("&#039;", "'")
-      .replaceAll("&eacute;", "é")
-      .replaceAll("&amp;", "&")
-      .replaceAll("&rsquo;", "`");
-  }
-
+  const [selectedBtn, setSelectedBtn] = useState("");
   const newQuestion = filterText(props.question.question);
   const correctAnswer = filterText(props.question.correct_answer);
   const incorrectAnswers = props.question.incorrect_answers.map((answer) => {
@@ -21,26 +12,69 @@ export default function Question(props) {
   allAnswers.push(correctAnswer);
   allAnswers.push(...incorrectAnswers);
 
-  //Function that randomly splices the allAnswers array into a new random array
-  //so that correct answers aren't predictably placed
-  let mixedAnswersArr = [];
-  function randomButtons() {
-    while (allAnswers.length > 0) {
-      let randomIndex = Math.floor(Math.random() * allAnswers.length);
-      let splicedAnswer = allAnswers.splice(randomIndex, 1);
-      mixedAnswersArr.push(...splicedAnswer);
-    }
-  }
-  randomButtons();
+  const [buttonsValue, setButtonValue] = useState([]);
+  const mixedAnswersArr = [];
 
+  function updateBtn(event) {
+    setSelectedBtn(event.target.textContent);
+  }
+
+  //This replaces the weird html entity syntax with the proper syntax.
+  function filterText(text) {
+    return text
+      .replaceAll("&quot;", '"')
+      .replaceAll("&#039;", "'")
+      .replaceAll("&eacute;", "é")
+      .replaceAll("&amp;", "&")
+      .replaceAll("&rsquo;", "`");
+  }
+
+  useEffect(() => {
+    //Function that randomly splices the allAnswers array into a new random array
+    //so that correct answers aren't predictably placed
+    function randomButtons() {
+      while (allAnswers.length > 0) {
+        let randomIndex = Math.floor(Math.random() * allAnswers.length);
+        let splicedAnswer = allAnswers.splice(randomIndex, 1);
+        mixedAnswersArr.push(...splicedAnswer);
+      }
+      setButtonValue(mixedAnswersArr);
+    }
+    randomButtons();
+  }, []);
+  console.log(buttonsValue);
   return (
     <div className="question-container">
       <h2>{newQuestion}</h2>
       <div className="button-container">
-        <button type="button">{mixedAnswersArr[0]}</button>
-        <button type="button">{mixedAnswersArr[1]}</button>
-        <button type="button">{mixedAnswersArr[2]}</button>
-        <button type="button">{mixedAnswersArr[3]}</button>
+        <button
+          type="button"
+          onClick={updateBtn}
+          className={buttonsValue[0] === selectedBtn ? "selected-btn" : ""}
+        >
+          {buttonsValue[0]}
+        </button>
+        <button
+          type="button"
+          onClick={updateBtn}
+          className={buttonsValue[1] === selectedBtn ? "selected-btn" : ""}
+        >
+          {buttonsValue[1]}
+        </button>
+        <button
+          type="button"
+          onClick={updateBtn}
+          className={buttonsValue[2] === selectedBtn ? "selected-btn" : ""}
+        >
+          {buttonsValue[2]}
+        </button>
+        <button
+          type="button"
+          onClick={updateBtn}
+          className={buttonsValue[3] === selectedBtn ? "selected-btn" : ""}
+        >
+          {buttonsValue[3]}
+        </button>
       </div>
     </div>
   );
